4973473faeec4f15a23854c8065b32c00720dd6e
william <z5311787@ad.unsw.edu.au>
2022-10-19 10:45:10 +1100

    attempt deploy

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,18 @@ import HomePage from './pages/HomePage';
 import ResumePage from './pages/ResumePage';
 import ProjectsPage from './pages/ProjectsPage';
 import BlogPage from './pages/AboutPage';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import {BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-            <Route
-              path="*"
-              element={<Navigate to={'/'} replace />}
-            ></Route>
-            <Route path="/" element={<HomePage />}></Route>
-            <Route path="/resume" element={<ResumePage />}></Route>
-            <Route path="/projects" element={<ProjectsPage />}></Route>
-            <Route path="/blog" element={<BlogPage />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/resume" element={<ResumePage />}></Route>
+          <Route path="/projects" element={<ProjectsPage />}></Route>
+      </Routes>
+
     </>
   );
 }
