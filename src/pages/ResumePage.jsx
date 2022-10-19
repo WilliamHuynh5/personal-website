f4b72bdc964ac4993a5365d82a13b6173b48c397
william <z5311787@ad.unsw.edu.au>
2022-10-19 17:58:18 +1100

    added resume

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../index.css';
 import NavBar from '../components/NavBar';
+import resume from '../assets/resume.png';
 
 
 const ResumePage = () => { 
@@ -10,6 +11,10 @@ const ResumePage = () => {
       <h1 className="tab-title">
         <span style={{fontWeight: 'bold'}}>my</span> resumè
       </h1> 
+      <div style={{textAlign: 'center'}}>
+        <img className="resume-image" src={resume}></img>
+      </div>
+      
     </>
   )
 }
